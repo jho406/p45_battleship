e705ec1caa4ac727b033088b3f5f4133b8c2a82f
Johny Ho <jho406@gmail.com>
2013-06-23 10:01:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P45Battleship::Application.config.secret_token = '00c75d28bdf60588810589712f55409b2a235814b31a91aeaed906ade3ead4a369f1a4c8b8a1e1662b18fe1954218f5dc4c1e39ed666ae464eea21268885522b'
