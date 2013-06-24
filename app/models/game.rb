de8f7dea6174ef1f2682a132a94956bd4713b442
johny <jho406@gmail.com>
2013-06-24 16:13:12 -0400

    Added battleship specific validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Game < ActiveRecord::Base
   has_many :deployments
   has_many :turns
@@ -8,4 +10,17 @@ class Game < ActiveRecord::Base
   accepts_nested_attributes_for :deployments, :turns
 
   validates :email, :full_name, :p45_id, :p45_response, :presence => true
+  validates :deployment_attributes,
+    :presence => true,
+    :length => {:is=>Battleship::SHIPS.length}
+  validate :deployment_ids_must_equal
+
+  private
+
+  def deployment_ids_must_equal
+    id_set = self.deployment_attributes.map{|ship|ship.ship_id}.to_set
+    if Battleship.ship_ids.to_set != id_set
+      errors.add(:deployment_attributes, "ship ids are not valid")
+    end
+  end
 end
