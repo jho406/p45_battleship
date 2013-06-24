5357bf895f4a8d620bc17f5a84f07b5cb8a38203
johny <jho406@gmail.com>
2013-06-24 16:03:39 -0400

    added missing turn association, some validations and removed status accessor.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,11 @@
 class Game < ActiveRecord::Base
   has_many :deployments
+  has_many :turns
   has_many :ships, :through => :deployments
 
-  attr_accessible :email, :full_name, :p45_id, :p45_response, :status
+  attr_accessible :email, :full_name, :p45_id, :p45_response
+
+  accepts_nested_attributes_for :deployments, :turns
+
+  validates :email, :full_name, :p45_id, :p45_response, :presence => true
 end
