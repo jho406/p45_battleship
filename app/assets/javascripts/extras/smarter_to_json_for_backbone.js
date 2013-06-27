db96e7e220d08fd4ad540a744cd7983928bad1c3
johny <jho406@gmail.com>
2013-06-27 13:33:26 -0400

    Overrode backbone sync and added support for adding a paramRoot to POST, PUT, DELETE, PATCH requests. Other libraries such as backbone-rails gem overrides the entire SYNC. I just simply added some functionality via options to toJSON.

---
+++ app/assets/javascripts/extras/smarter_to_json_for_backbone.js
@@ -0,0 +1,27 @@
+!function(Backbone){
+  var sync = Backbone.sync;
+  Backbone.sync = function( method, model, options ) {
+
+    if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
+      options.includeParamRoot = true;
+    }
+
+    return sync.apply(this, [method, model, options]);
+  };
+
+  _.extend(Backbone.Model.prototype, {
+    toJSON: function(options) {
+      var data = {},
+          attrs = _.clone(this.attributes);
+
+      if(options.includeParamRoot && this.paramRoot) {
+        data[this.paramRoot] = attrs;
+      } else {
+        data = attrs;
+      }
+
+      return data;
+    }
+  });
+
+}(Backbone);
