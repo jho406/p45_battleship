a2de6bc06e2352eee0f1d061ec69a88e6bbf2313
johny <jho406@gmail.com>
2013-06-28 18:45:36 -0400

    added validation to models

--- app/models/deployment.rb
+++ app/models/deployment.rb
@@ -1,6 +1,25 @@
 class Deployment < ActiveRecord::Base
   belongs_to :game
   belongs_to :ship
+  #todo: why do i need orientation?
+  attr_accessible :ship_id, :orientation, :positions
 
-  attr_accessible :lives, :orientation, :position
+  before_save :add_lives
+  before_validation :expand_positions
+
+  validates :ship_id, :orientation, :positions, :presence => true
+
+  private
+
+  def add_lives
+    self.lives = self.ship.length
+  end
+
+  def expand_positions
+    self.positions = Battleship.expand_pos(
+      :position => self.positions.first,
+      :length    => self.ship.length #extra query here..
+    )
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -5,22 +5,43 @@ class Game < ActiveRecord::Base
   has_many :turns
   has_many :ships, :through => :deployments
 
-  attr_accessible :email, :full_name, :p45_id, :p45_response
+  attr_accessible :email, :full_name,
+    :p45_id, :p45_response,
+    :deployments_attributes, :turns_attributes
 
   accepts_nested_attributes_for :deployments, :turns
 
   validates :email, :full_name, :p45_id, :p45_response, :presence => true
-  validates :deployment_attributes,
+  validates :email,
+    :format => { :with=> /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
+
+  validates :deployments,
     :presence => true,
-    :length => {:is=>Battleship::SHIPS.length}
-  validate :deployment_ids_must_equal
+    :length => {
+      :is=>Battleship::SHIPS.length,
+      :message=> "All ships need to be deployed"}
+  validates :turns, :presence => true
+
+  validate :ships_must_exist_and_be_unique, :positions_must_not_collide
 
   private
 
-  def deployment_ids_must_equal
-    id_set = self.deployment_attributes.map{|ship|ship.ship_id}.to_set
-    if Battleship.ship_ids.to_set != id_set
-      errors.add(:deployment_attributes, "ship ids are not valid")
+
+  def ships_must_exist_and_be_unique
+    id_set = self.deployments.map(&:ship_id).to_set
+    ##todo: inject the ship model?
+    unless Battleship.ship_ids.to_set == id_set
+      errors.add(:deployments_attributes, "ship ids are not valid")
     end
+  end
+
+  def positions_must_not_collide
+    all_positions = deployments.inject([]) do |memo, ship|
+      # ship.expand_positions
+      memo += ship.positions
+    end
+
+    collision = !all_positions.group_by { |pos| pos }.select { |k, v| v.size > 1 }.empty?
+    errors.add(:deployment_positions, "ships overlap") if collision
   end
 end
