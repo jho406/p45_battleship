f666c36dc313f1d1e25fb2e267ecffc3a0cd1940
johny <jho406@gmail.com>
2013-06-30 21:50:35 -0400

    fixed up battleship to support nuking. could use a refactor.

--- app/services/battleship.rb
+++ app/services/battleship.rb
@@ -12,7 +12,7 @@ module Battleship
     {:name=>"Patrol Boat",:length=>1}
   ]
 
-  STEP = {:horizontal => 1, :vertical=>10}
+  STEP = {:horizontal => 1, :vertical => 10}
 
   extend self
 
@@ -42,7 +42,7 @@ module Battleship
 
     game.valid?
     # debugger
-    return game if game.errors.keys.to_set != [:p45_id, :turns, :p45_response].to_set
+    return game if game.errors.keys.to_set != [:p45_id, :turns].to_set
 ##todo: module include in model, it has to know something about turn and game...
 ## have it be included in both models rather than depending on dependancy injection...
     api = P45.new({:email => email, :name => name})
@@ -58,15 +58,43 @@ module Battleship
     return game
   end
 
-  def nuke!(game, index)
-    api = P45.new(:id=>game.id)
+  def nuke!(args)
+    game = args[:game]
+    index = args[:position]
+
+    return game if game.over?
+    api = P45.new(:id => game.p45_id)
 
     api.nuke(pos_to_coord(index))
-    game.turn.create!(:position=>index)
+    game.turns.create!(:position=>index, :status=> api.response['status'], :attacked=>true)
+    #which creates a turn hit/miss # todo, add a transformer
 
-    coord = transform_coord(api.response)
-    index = coord_to_pos(coord)
-    game.turn.create!(:position=>index)
+    #check if i won, aka if its game over for them but not us
+    if api.response['status'] == 'over'
+      game.win!
+      return game
+    end
+
+    #i didn't win yet, i also receive a hit
+    #todo: eager load assoications via the controller
+    index = coord_to_pos(transform_coord(api.response))
+    recieve_nuke!(game, index)
+
+    return game
+  end
+
+  def recieve_nuke!(game, index)
+    locked_on_ship = game.deployments.lock_on(index)
+    status = damage_and_report!(locked_on_ship)
+    game.lose! if game.over?
+    #finally create the turn, but I didn't attack this turn so attacked == false
+    game.turns.create!(:position=>index, :status => status, :attacked=>false)
+  end
+
+  def damage_and_report!(ship)
+    return 'miss' if !ship
+    ship.damage! #decrement the ship that got hit careful for less than zero
+    return "hit"
   end
 
   def coord_to_pos(coord)
@@ -75,7 +103,7 @@ module Battleship
     end
 
     coord = coord.to_options
-    coord[:x] * BOARD_SIZE + coord[:y]
+    coord[:y] * BOARD_SIZE + coord[:x]
   end
 
   def pos_to_coord(index)
