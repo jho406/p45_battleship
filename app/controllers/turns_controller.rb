75547171d90a7847141ea2c57ef7c118bd48254a
johny <jho406@gmail.com>
2013-06-30 20:16:06 -0400

    backbone expects a body response for everything.. so reply with empty hash for now.

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -7,6 +7,10 @@ class TurnsController < ApplicationController
 
   def create
     Battleship.nuke!(params[:turn].merge(:game => current_game))
-    head :created
+    #backbone expects a response in the body, since a turn get created
+    #from the backend also, we have no need to return something
+    #instead we have to ping index. todo: make this more efficient by returning
+    #the entire index instead of black json
+    render :json=>{}.to_json, :status=>:created
   end
 end
